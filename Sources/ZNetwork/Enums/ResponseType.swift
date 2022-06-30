3807e51f52d0709334714c10f4a03169043f5d99
Jeeva <azhagusundaram.t@zohocorp.com>
2022-06-30 15:45:32 +0530

    response type

---
+++ Sources/ZNetwork/Enums/ResponseType.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Azhagusundaram Tamil on 30/06/22.
+//
+
+import Foundation
+
+/// The expected remote response type.
+public enum ResponseType {
+    /// Used when the expected response is a JSON payload.
+    case json
+    /// Used when the expected response is a file.
+    case file
+}
